0121d254ba7de4f878db637502d7267a11d11982
forellko <dragonforce97q@gmail.com>
2024-08-06 10:26:23 +0300

    Create header

---
+++ components/shared/header.tsx
@@ -0,0 +1,10 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+type Props = {};
+
+function Header({}: Props) {
+  return <header className={cn('border border-b')}></header>;
+}
+
+export default Header;
