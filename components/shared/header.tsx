35256b63ecae99464db084f36cd79efa806456f6
forellko <dragonforce97q@gmail.com>
2024-08-06 10:41:04 +0300

    Create header left

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,10 +1,27 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
+import { Container } from './container';
+import Image from 'next/image';
 
 type Props = {};
 
 function Header({}: Props) {
-  return <header className={cn('border border-b')}></header>;
+  return (
+    <header className={cn('border border-b')}>
+      <Container className="flex items-center justify-center py-8">
+        {/* Левая часть */}
+        <div>
+          <Image src={'/logo.png'} alt="logo" width={35} height={35} />
+          <div>
+            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+            <p className="text-sm text-gray-400 leading-3">
+              вкусней уже некуда
+            </p>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
